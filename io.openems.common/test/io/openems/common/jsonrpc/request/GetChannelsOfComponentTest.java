91a728863e6ccc081bcc398cf37c570667de158c
Lamarqe <scritch@gmx.de>
2026-05-14 20:43:58 +0200

    extend test with enum option channel

--- io.openems.common/test/io/openems/common/jsonrpc/request/GetChannelsOfComponentTest.java
+++ io.openems.common/test/io/openems/common/jsonrpc/request/GetChannelsOfComponentTest.java
@@ -7,6 +7,9 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
 import io.openems.common.channel.AccessMode;
 import io.openems.common.channel.ChannelCategory;
 import io.openems.common.channel.PersistencePriority;
@@ -27,12 +30,30 @@ public class GetChannelsOfComponentTest {
 	@Test
 	public void testResponseSerializer() {
 		// Create a sample channel record with string options
-		List<String> stringOptions = Arrays.asList("VERY_LOW", "LOW", "MEDIUM", "HIGH", "VERY_HIGH");
-		var channelRecord = new GetChannelsOfComponent.ChannelRecord(//
-				"priority", //
+		List<GetChannelsOfComponent.ChannelRecord.OptionsEnumEntry> options = Arrays.asList(//
+				new GetChannelsOfComponent.ChannelRecord.OptionsEnumEntry("Ok", 0), //
+				new GetChannelsOfComponent.ChannelRecord.OptionsEnumEntry("Info", 1), //
+				new GetChannelsOfComponent.ChannelRecord.OptionsEnumEntry("Warning", 2), //
+				new GetChannelsOfComponent.ChannelRecord.OptionsEnumEntry("Fault", 3)//
+		);
+		var plainChannelRecord = new GetChannelsOfComponent.ChannelRecord(//
+				"State", //
+				AccessMode.READ_ONLY, //
+				PersistencePriority.VERY_HIGH, //
+				"0:Ok, 1:Info, 2:Warning, 3:Fault", //
+				OpenemsType.INTEGER, //
+				Unit.NONE, //
+				ChannelCategory.ENUM, //
+				null, // level
+				null, //stringOptions
+				options //
+		);
+		List<String> stringOptions = Arrays.asList("NONE", "DEBUG_LOG", "TRACE");
+		var attributedefinitionRecord = new GetChannelsOfComponent.ChannelRecord(//
+				"_PropertyLogVerbosity", //
 				AccessMode.READ_WRITE, //
 				PersistencePriority.HIGH, //
-				"Priority Level", //
+				"", //
 				OpenemsType.STRING, //
 				Unit.NONE, //
 				ChannelCategory.OPENEMS_TYPE, //
@@ -42,19 +63,21 @@ public class GetChannelsOfComponentTest {
 		);
 
 		// Create a response with the channel record
-		var originalResponse = new GetChannelsOfComponent.Response(Arrays.asList(channelRecord));
+		var originalResponse = new GetChannelsOfComponent.Response(Arrays.asList(plainChannelRecord, attributedefinitionRecord));
 
-		// Serialize to JSON
+		// Serialize to JSON string
 		var json = GetChannelsOfComponent.Response.serializer().serialize(originalResponse);
+		String jsonString = json.toString();
 
-		// Deserialize back from JSON
-		var result = GetChannelsOfComponent.Response.serializer().deserialize(json);
+		// Deserialize back from JSON string
+		JsonElement stringJson = JsonParser.parseString(jsonString);
+		var result = GetChannelsOfComponent.Response.serializer().deserialize(stringJson);
 
 		// Verify the deserialized response matches the original
 		assertEquals(originalResponse, result);
-		assertEquals(1, result.channels().size());
-		assertEquals("priority", result.channels().get(0).id());
-		assertEquals(stringOptions, result.channels().get(0).stringOptions());
+		assertEquals(2, result.channels().size());
+		assertEquals("State", result.channels().get(0).id());
+		assertEquals(stringOptions, result.channels().get(1).stringOptions());
 	}
 
 }
